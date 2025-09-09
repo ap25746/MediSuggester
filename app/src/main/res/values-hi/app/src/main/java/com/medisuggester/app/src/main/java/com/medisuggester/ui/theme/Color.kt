15e0e821315accd1bb9a05fa1a9ee215ec3de438
ap25746 <shakilahmad16889@gmail.com>
2025-09-09 14:28:15 +0530

    Create Color.kt

---
+++ app/src/main/res/values-hi/app/src/main/java/com/medisuggester/app/src/main/java/com/medisuggester/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.medisuggester.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenPrimary = Color(0xFF2E7D32)
+val GreenSecondary = Color(0xFF66BB6A)
+val GreenAccent = Color(0xFF43A047)
+val BackgroundLight = Color(0xFFF6FFF6)
